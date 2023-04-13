60429e7dfaf202c90b9489ce8be185d24e91f0a8
deva <kbdevaprasadkb22@gmail.com>
2023-04-13 07:39:32 +0530

    deleted all files

--- one.java
+++
@@ -1,7 +0,0 @@
-// Your First Program
-// Adding message
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, Welcome to DevOps Training"); 
-    }
-}
